09db258c57e6ab95f3af68f293975ead0bca3057
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-11-09 09:36:31 +0700

    update

--- src/pages/oprec.tsx
+++ src/pages/oprec.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import StepFlow from './stepflow';
-import Navbar from './components/navbar';
-import StepperTailwind from './stepper';
-import useResponsive from '@/components/useResponsive'
+import React from "react";
+import StepFlow from "./stepflow";
+import Navbar from "./components/navbar";
+import StepperTailwind from "./stepper";
+import useResponsive from "@/components/useResponsive";
 const Oprec = () => {
-  const {isDesktop} = useResponsive()
+  const { isDesktop } = useResponsive();
   return (
-    <div className='w-full flex flex-col'>
-       <Navbar />
-    <div className="min-h-screenc w-full flex justify-center items-center bg-cover bg-center" style={{backgroundImage: "url('/bg.png')"}}>
-      
-      {/* <div className='md:w-1/2 md:px-20 sm:w-full bg-white py-16 md:m-20 sm:m-0 rounded-3xl'>
+    <div className="">
+      <Navbar />
+      <div
+        className="min-h-screen w-full flex justify-center items-center bg-cover bg-center"
+        style={{ backgroundImage: "url('/bg.png')" }}
+      >
+        {/* <div className='md:w-1/2 md:px-20 sm:w-full bg-white py-16 md:m-20 sm:m-0 rounded-3xl'>
         
         <StepperTailwind />
       </div> */}
-      {isDesktop ?
-      
-      <div className='bg-white py-16 w-1/2 px-20 m-20 rounded-3xl'>
-      <StepperTailwind></StepperTailwind>
-    </div>
-    :
-    <div className='bg-white py-16 w-full p-10 rounded-3xl'>
-        <StepperTailwind></StepperTailwind>
+        {isDesktop ? (
+          <div className="bg-white py-16 w-1/2 px-20 m-20 rounded-3xl">
+            <StepperTailwind></StepperTailwind>
+          </div>
+        ) : (
+          <div className="bg-white py-16 my-10 w-[90%]   p-10 rounded-3xl">
+            <StepperTailwind></StepperTailwind>
+          </div>
+        )}
       </div>
-    }
     </div>
-
-    </div>
-   
   );
-}
+};
 
 export default Oprec;
