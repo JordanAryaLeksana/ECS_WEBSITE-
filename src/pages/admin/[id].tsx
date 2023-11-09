136fbf730523ea47244367d57c7c0d433fe7981b
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-11-09 18:51:25 +0700

    update

---
+++ src/pages/admin/[id].tsx
@@ -0,0 +1,88 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
+import { auth, db } from '@/firebase/firebase_config';
+
+const Admin = () => {
+  const { query, push } = useRouter();
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (query.id === 'tes') {
+          console.log('tes');
+        } else {
+          const querySnapshot = await getDocs(collection(db, 'users'));
+          const userData = querySnapshot.docs.map((doc) => doc.data());
+          setData(userData);
+        }
+      } catch (error) {
+        console.error('Error getting documents: ', error);
+      }
+    };
+
+    fetchData();
+  }, [query.id, push]);
+
+  return (
+    <div className='w-full min-h-screen' style={{ backgroundImage: "url('/bg.png')", backgroundSize: 'contain', backgroundRepeat: 'repeat' }}>
+      {query.id !== 'login-admin-ecs-success--admin' ? (
+        <div className="text-center py-8">Tidak ada apa-apa di sini</div>
+      ) : (
+        <div className="mx-auto max-w-screen-lg p-8">
+          <h1 className='text-center font-bold text-2xl pt-10 mb-8'>
+            Data Pendaftar Oprec ECS 2024
+          </h1>
+          {data && (
+            <div className="overflow-x-auto">
+              <table className="w-full table-auto border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-2">No</th>
+                    <th className="px-4 py-2">Nama</th>
+                    <th className="px-4 py-2">Email</th>
+                    <th className="px-4 py-2">NRP</th>
+                    <th className="px-4 py-2">CV</th>
+                    <th className="px-4 py-2">MotLet</th>
+                    <th className="px-4 py-2">SK</th>
+                    <th className="px-4 py-2">Pas Foto</th>
+                    <th className="px-4 py-2">KTM</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item: any, index: any) => (
+                    <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
+                      <td className="px-4 py-2">{index + 1}</td>
+                      <td className="px-4 py-2">{item.name}</td>
+                      <td className="px-4 py-2">{item.email}</td>
+                      <td className="px-4 py-2">{item.nrp}</td>
+                      <td className="px-4 py-2 text-center">
+                        <a className='p-3 py-1 bg-yellow-500 rounded-2xl' href={item.cv} target="_blank" rel="noopener noreferrer">
+                          Open
+                        </a>
+                      </td>
+                      <td className="px-4 py-2 text-center">-</td>
+                      <td className="px-4 py-2 text-center">
+                        <a className='p-3 py-1 bg-yellow-500 rounded-2xl' href={item.foto} target="_blank" rel="noopener noreferrer">
+                          Open
+                        </a>
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        <a className='p-3 py-1 bg-yellow-500 rounded-2xl' href={item.ktm} target="_blank" rel="noopener noreferrer">
+                          Open
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Button, Input, Typography } from "@material-tailwind/react"; // Import your Input component
+import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css'
+const Admin = () => {
+    const username = 'admin';
+    const password = 'adminecsoke';
+    const [inputUsername, setInputUsername] = React.useState('');
+    const [inputPassword, setInputPassword] = React.useState('');
+    const [isLogin, setIsLogin] = React.useState(false);
+    const secret_key = 'login-admin-ecs-success--admin';
+    const { push } = useRouter();
+
+    const handleLogin = () => {
+        if (inputUsername === username && inputPassword === password) {
+            push(`/admin/${secret_key}`);
+        }
+        else {
+            toast.error("Wrong username or password")
+        }
+    };
+
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen ' style={{backgroundImage: "url('/bg.png')", backgroundSize: 'contain', backgroundRepeat: 'repeat'}}>
+
+            <div className='flex flex-col  space-y-4 lg:w-1/2 sm:w-full text-left'>
+                <Typography slot='' variant="h6" color="blue-gray" className="-mb-3">
+                    Username
+                </Typography>
+                <Input
+                    onChange={(e) => setInputUsername(e.target.value)}
+                    value={inputUsername}
+                    type="text"
+                    size="lg"
+                    placeholder="Enter your username"
+                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                    labelProps={{
+                        className: "before:content-none after:content-none",
+                    }} crossOrigin={"anonymous"} />
+
+                <Typography variant="h6" color="blue-gray" className="-mb-3">
+                    Password
+                </Typography>
+                <Input
+                    crossOrigin={"anonymous"}
+                    onChange={(e) => setInputPassword(e.target.value)}
+                    value={inputPassword}
+                    type="password"
+                    size="lg"
+                    placeholder="Enter your password"
+                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                    labelProps={{
+                        className: "before:content-none after:content-none",
+                    }}
+                />
+
+                <Button className='w-full'
+                    onClick={handleLogin}
+
+                >
+                    Login
+                </Button>
+            </div>
+            <ToastContainer></ToastContainer>
+        </div>
+    );
+}
+
+export default Admin;
