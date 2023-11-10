31bab55c8255004cfed6dd1c4ac69d3a6e0f4333
dzulfikarubaid <macbook@MacBooks-MacBook-Pro.local>
2023-11-10 10:47:58 +0700

    update

--- src/pages/oprec.tsx
+++ src/pages/oprec.tsx
@@ -24,7 +24,7 @@ const Oprec = () => {
             <StepperTailwind></StepperTailwind>
           </div>
         ) : (
-          <div className="bg-white py-8 my-10 w-full mx-6 p-8 rounded-3xl h-fit">
+          <div className="bg-white py-8 my-10 w-full p-8 rounded-3xl h-fit">
             <StepperTailwind></StepperTailwind>
           </div>
         )}
