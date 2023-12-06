2c88d0b159ab20c42921c322bfef7ef77894b5c4
dzulfikarubaid <dzulfikarubaid@gmail.com>
2023-12-07 04:08:55 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/navbar'
-import Link from 'next/link'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
-import useResponsive from '@/components/useResponsive'
-import { duration } from '@mui/material'
-import {InfinitySpin} from "react-loader-spinner"
-import Head from 'next/head'
-const loadingContainer = {
-  display: "flex"
-  ,
-  width: "2rem"
-  ,
-  height: "2rem",
-  justifyContent: "space-around"
-}
-const loadingCircle = {
-  display: "block"
-  ,
-  width: "0.5rem"
-  ,
-  height: "0.5rem",
-  backgroundColor: "black",
-  borderRadius: "0.25rem"
+import React from 'react';
+import { Button, Input, Typography } from "@material-tailwind/react";
+import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
+import Head from 'next/head';
+import { unstable_noStore as noStore} from 'next/cache';
 
-}
-const loadingCircleVariants = {
-  start: {
-    y: "0%"
+const TekaTeki = () => {
+    noStore();
+    const answer1 = process.env.SERVER_ANSWER1;
+    const answer2 = process.env.SERVER_ANSWER2;
+    const answer3 = process.env.SERVER_ANSWER3;
+    console.log(answer1, answer2, answer3)
+    const [inputAnswer1, setInputAnswer1] = React.useState('');
+    const [inputAnswer2, setInputAnswer2] = React.useState('');
+    const [inputAnswer3, setInputAnswer3] = React.useState('');
+    const [isLogin, setIsLogin] = React.useState(false);
+    const secretKey = process.env.NEXT_PUBLIC_DR;
+    const { push } = useRouter();
 
-  },
-  end: {
-    y: "100%"
-  }
-}
-const loadingCircleTransition = {
-  duration:1,
-  yoyo: Infinity,
-  ease: "easeInOut"
-}
-const loadingContainerVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.1
-    }
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.1
-    }
-  }
-}
-const Home = () => {
-  const { isDesktop, isMobile } = useResponsive()
-  const [show, setShow] = useState(false);
+    const handleLogin = () => {
+        if(inputAnswer1 != answer1){
+            toast.error("Wrong answer 1")
+            return;
+        }
 
-  useEffect(
-    () => {
-      let timer1 = setTimeout(() => setShow(true), 2000);
-      return () => {
-        clearTimeout(timer1);
-      };
-    },
-    []
-  );
-  return (
-    show ?
-      <div >
-      <Head><title>ecs-laboratory</title></Head>
-        <Navbar></Navbar>
-        <div className='flex flex-row justify-center w-full items-center lg:px-20 sm:px-10  min-h-screen' style={{ backgroundImage: "url('/bg.png')", backgroundSize: 'contain', backgroundRepeat: 'repeat' }}>
-          {
-            isDesktop ?
-              <div className='border-dashed border-[3px] py-20 px-10 border-black/30 flex flex-row  justify-center items-center '>
-                <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.7, ease: 'easeInOut' }} width={380} height={380} src="/ecswarna.png" alt="" />
-                <div className='flex flex-col gap-6 w-[800px]'>
-                  <h1 className='text-kuning font-extrabold text-[40px]' style={{ WebkitTextStroke: '1px gray' }}>EMBEDDED & CYBER PHYSICAL SYSTEM LABORATORY</h1>
-                  <h1 className='font-semibold'>Merupakan laboratorium yang menghubungkan gagasan penelitian terbaru berbasis fisika dengan pengembangan produk atau proses dalam format <span className='font-light'>embedded system </span> dan skema <span className='font-light'>internet-of-things</span>.</h1>
-                  <div className='flex flex-row gap-4'>
-                    <Link className='px-6 py-2 rounded-2xl bg-white' href={'/about'} >About</Link>
-                    <Link className='px-6 py-2  rounded-2xl bg-gray-800 text-kuning' href={'/oprec'}>Join Us</Link>
-                  </div>
+        if(inputAnswer2 != answer2){
+            toast.error("Wrong answer 2")
+            return;
+        }
+
+        if(inputAnswer3 != answer3){
+            toast.error("Wrong answer 3")
+            return;
+        }
+
+        toast.success("Oke benar")
+    };
+
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen ' style={{backgroundImage: "url('/bg.png')", backgroundSize: 'contain', backgroundRepeat: 'repeat'}}>
+        {/* <h1>{answer1}</h1>
+        <h1>{answer2}</h1>
+        <h1>{answer3}</h1> */}
+            <div className='flex flex-col gap-6 justify-center items-center text-left w-fit'>
+                <div className='flex flex-row gap-4 justify-start items-center w-full'>
+                    <h1>Makanan favoritku adalah </h1> 
+                    <input 
+                        type="text" 
+                        className='bg-transparent border-b-2 border-black focus:outline-none w-[140px]' 
+                        maxLength={15}
+                        value={inputAnswer1}
+                        onChange={(e) => setInputAnswer1(e.target.value)}
+                    /> 
+                    <h1>karena </h1>
                 </div>
-
-              </div>
-              :
-              <div className='border-dashed  py-20 px-10 border-black/30 flex flex-col  justify-center items-center '>
-                <motion.img initial={{ y: 0 }} animate={{ y: 0 }} width={250} height={250} src="/ecswarna.png" alt="" />
-                <div className='flex flex-col gap-6 w-full'>
-                  <h1 className='text-kuning font-extrabold text-[32px]' style={{ WebkitTextStroke: '1px gray' }}>EMBEDDED & CYBER PHYSICAL SYSTEM LABORATORY</h1>
-                  <h1 className='font-semibold'>Merupakan laboratorium yang menghubungkan gagasan penelitian terbaru berbasis fisika dengan pengembangan produk atau proses dalam format <span className='font-light'>embedded system </span> dan skema <span className='font-light'>internet-of-things</span>.</h1>
-                  <div className='flex flex-row gap-4'>
-                    <Link className='px-6 py-2 rounded-2xl bg-white' href={'/about'} >About</Link>
-                    <Link className='px-6 py-2  rounded-2xl bg-gray-800 text-kuning' href={'/oprec'}>Join Us</Link>
-                  </div>
+                <div className='flex flex-row gap-4 justify-start items-center w-full'>
+                    <div className='flex flex-row'>
+                        <input 
+                            type="text" 
+                            className='bg-transparent border-b-2 border-black focus:outline-none w-[80px]' 
+                            maxLength={7}
+                            value={inputAnswer2}
+                            onChange={(e) => setInputAnswer2(e.target.value)}
+                        />
+                        <h1>nya</h1>
+                    </div>
+                    <input 
+                        type="text" 
+                        className='bg-transparent border-b-2 border-black focus:outline-none w-[80px]' 
+                        maxLength={10}
+                        value={inputAnswer3}
+                        onChange={(e) => setInputAnswer3(e.target.value)}
+                    /> 
                 </div>
-
-              </div>
-          }
-
-
+                <Button className='w-full' onClick={handleLogin}>
+                    Oke
+                </Button>
+            </div>
+            <ToastContainer></ToastContainer>
         </div>
-
-      </div>
-      :
-      <motion.div transition={{ duration: 1 }} animate={{ y: 0 }} initial={{ y: 0 }} exit={{ y: '-100%' }} className='w-full h-screen flex justify-center items-center'>
-      <InfinitySpin 
-  width='200'
-  color='black'
-  
-/>
-      </motion.div>
-  )
+    );
 }
 
-export default Home
+export default TekaTeki;
