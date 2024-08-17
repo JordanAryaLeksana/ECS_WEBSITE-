430689dabd8bf5a91580b1eee80133fb2de80b21
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2024-08-18 01:23:45 +0700

    semoga bisa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
-  reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
   }
