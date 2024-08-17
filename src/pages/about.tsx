ef56291ce77397978274ee05ce55a829bc09bf0e
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2024-08-18 00:53:16 +0700

    updates error deploy

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,15 +11,11 @@ const About = () => {
       <div className='h-full min-h-screen w-screen bg-black  '>
       
           <Card Variant='GlassHover' Header='Embedded System' Paragraph='Embedded System merupakan devisi yang yang mendalami sistem kontrol yang dirancang spesifik untuk fungsi tertentu menggunakan perangakat keras dan perangkat lunak komputer
-         yang diintegrasikan agar produk yang dihasilkan lebih mudah dan efisien.'  ImageSrc='/ComponentCard1.svg' ImageAlt='logo' Width={50} Height={50} className=''>
-          </Card>
+         yang diintegrasikan agar produk yang dihasilkan lebih mudah dan efisien.'  ImageSrc='/ComponentCard1.svg' ImageAlt='logo' Width={50} Height={50} className=''/>
 
-          <Card  Icons={<HiCode className='' size={30}/>} Variant='PaymentCard' Header='Dasar Pemprograman' Paragraph='Rp.000,000,00' >
-          </Card>
+          <Card  Icons={<HiCode className='' size={30}/>} Variant='PaymentCard' Header='Dasar Pemprograman' Paragraph='Rp.000,000,00' />
 
-          <Card Variant='AssistantCard'  Header='Alif A.' Paragraph='Artificial Intelegent' AddImage ImageAlt='jordan' ImageSrc='/alif.png' Width={150} Height={150}>
-
-          </Card>
+          <Card Variant='AssistantCard'  Header='Alif A.' Paragraph='Artificial Intelegent' AddImage ImageAlt='jordan' ImageSrc='/alif.png' Width={150} Height={150}/>
         <Button variant='default'>Button1</Button>
       </div>
     </Layout>
