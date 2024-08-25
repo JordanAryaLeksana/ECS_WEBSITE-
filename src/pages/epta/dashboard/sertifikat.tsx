304434bb28186b6a78efb97cd7ee6296ad42fdf1
unknown <fernandowijaya365@gmail.com>
2024-08-25 10:05:13 +0700

    update responsive sertifikat

--- src/pages/epta/dashboard/sertifikat.tsx
+++ src/pages/epta/dashboard/sertifikat.tsx
@@ -7,38 +7,47 @@ import { useState } from 'react';
 
 const Sertifikat = () => {
   const [isTrainingComplete, setIsTrainingComplete] = useState(true);
+
   return (
     <DashboardLayout>
-      <div className='absolute top-[45px] left-[350px]'>
-        <Typography variant='Paragraph' size='2xl' className='text-AddsOn-neutral font-normal'>
+      <div className='flex flex-col sm:ml-[40px] sm:mt-[45px]'>
+        <Typography variant='Paragraph' size='lg' className='text-AddsOn-neutral font-normal mb-6 sm:text-2xl leading-[36px]'>
           Sertifikat
         </Typography>
         {isTrainingComplete ? (
-          <div className='relative w-[912px] h-[94px] rounded-[24px] border-b border-[#000000]'>
-            <Image src='/logo unduh.svg' alt='icon' width={44} height={44} className=' absolute top-[25px] left-[32px]' />
-            <Typography variant='Paragraph' size='lg' className='font-semibold text-AddsOn-neutral absolute top-[25px] left-[124px]'>
-              Andre Alfa Jones Rawang
-            </Typography>
-            <Typography variant='Paragraph' size='sm' className='text-AddsOn-neutral font-normal absolute top-[58px] left-[124px]'>
-              Sertifikat
-            </Typography>
+          <div className='flex flex-col sm:flex-row sm:items-center w-full sm:max-w-[912px] h-[70px] sm:h-[94px] rounded-[24px] border-b border-[#000000] bg-[#1E1E1E] sm:p-5'>
+          <div className='flex sm:items-center w-full sm:w-auto'>
+            <div>
+            <Image src='/logo unduh.svg' alt='icon' width={29} height={29} className='ml-4 sm:mr-8 sm:mb-0 sm:w-[44px] sm:h-[44px] mt-1' />
+            </div>
+            <div className='flex flex-col ml-4 sm:ml-8'>
+              <Typography variant='Paragraph' size='xs' className='font-semibold text-AddsOn-neutral whitespace-nowrap sm:text-lg sm:leading-[27px] mt-[2px]'>
+                Andre Alfa Jones Rawang
+              </Typography>
+              <Typography variant='Paragraph' size='xs' className='text-AddsOn-neutral sm:font-light font-normal sm:text-sm sm:leading-[21px] leading-[24px]'>
+                Sertifikat
+              </Typography>
+            </div>
+          </div>
+          <div className='sm:ml-auto sm:mt-0 sm:mr-10 mx-[60px] '>
             <Link href={'/'}>
-              <Image src='/download icon.svg' alt='icon' width={24} height={24} className='absolute top-[35px] left-[826px]' />
+              <Typography variant='Paragraph' size='xs' className='text-primary-light-active underline sm:hidden'>
+                Download
+              </Typography>
+              <Image src='/download icon.svg' alt='icon' width={24} height={24} className='hidden sm:block' />
             </Link>
           </div>
+        </div>        
         ) : (
-          <div className='relative w-[912px] h-[21px]'>
-            <Typography variant='Paragraph' size='sm' className='text-primary-light-active font-normal absolute top-[40px]'>
+          <div className='w-full max-w-[912px] h-[21px] flex sm:items-center'>
+            <Typography variant='Paragraph' size='sm' className='text-primary-light-active font-normal'>
               Anda belum menyelesaikan course anda
             </Typography>
           </div>
-
         )}
       </div>
     </DashboardLayout>
-
   );
 };
 
-
-export default Sertifikat
+export default Sertifikat;
