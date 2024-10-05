4315607950079097090918734b2f3b2dbb98abfc
dzulfikarubaid <dzulfikarubaid@gmail.com>
2024-10-05 15:12:40 +0700

    update a to Link

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,6 +2,7 @@
 import Layout from '@/components/layout/Layout'
 import React from 'react'
 import Image from 'next/image'
+import ComingSoon from './comingsoon'
 const Project = () => {
     return (
         <Layout>
@@ -12,7 +13,9 @@ const Project = () => {
                 objectFit="cover"
                 className="fixed inset-0 block"
             />
-            <div className='h-screen w-screen bg-primary-normal-normal'></div>
+            <div className='h-screen w-screen bg-primary-normal-normal'>
+            <ComingSoon></ComingSoon>
+            </div>
         </Layout>
     )
 }
